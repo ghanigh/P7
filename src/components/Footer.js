b6ff910f094d02b8c8665f9c7062d77fd82491eb
Rhani M <rhani.contact@gmail.com>
2022-10-12 19:41:26 +0200

    commit ajout de photo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <div className="footer_container">
         <img src={footerLogo} alt="footer-logo" />
-        <p>&#xA9; 2020 Kasa. All rights reserved</p>
+        <p>&#xA9; 2022 Kasa. by Rhani</p>
       </div>
     </footer>
   );
