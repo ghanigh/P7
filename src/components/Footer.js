9520b6dea6267db51f8f7e2c21d81e0498132e0a
Rhani M <rhani.contact@gmail.com>
2023-01-17 18:50:36 +0100

    footer correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <div className="footer_container">
         <img src={footerLogo} alt="footer-logo" />
-        <p>&#xA9; 2022 Kasa. by Rhani</p>
+        <p>&#xA9; 2022 Kasa. by Ghani</p>
       </div>
     </footer>
   );
